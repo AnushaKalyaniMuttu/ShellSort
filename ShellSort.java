b618f2f4c0fd00904ff8fed15229027eba4cfd2c
Anusha Kalyani Muttu <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2023-10-07 23:56:59 +0530

    Update ShellSort.java

--- ShellSort.java
+++ ShellSort.java
@@ -2,7 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class ShellSort {
+@SpringBootApplication
+public class ScheduledApp2Application {
 
 	public static void main (String[] args) {
 	int[] arr= {4,-12,8,9,1,3,13,-2,9};
@@ -12,6 +13,8 @@ public class ShellSort {
 	for(int gap=arr.length/2;gap>0;gap/=2) {
 		
 		for(int i=gap;i<arr.length;i++) {
+			print(arr);
+			System.out.println();
 			int newElement=arr[i];
 			
 			int j=i;
